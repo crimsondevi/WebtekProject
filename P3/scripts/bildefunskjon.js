544f0fb3280cdd36b6990890e5b734fb2fbdcaa7
Markus Laptop <markusjpedersen@hotmail.com>
2018-11-01 11:52:00 +0100

    la til pizza bilder og omoss og bildefunksjon.js

---
+++ P3/scripts/bildefunskjon.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const pzaImg = document.getElementById('pza');
+  const pzaImg2 = document.getElementById('pza2');
+  var body = document.getElementById('body');
+  //pictureCount er mengden bilder til bildefremvisningen.
+  //Hvis man legger til flere bilder må man endre tallet
+  //bildene må også følge navn stilen pizza + tall .jpg
+  var pictureCount = 6;
+
+  //changePza velger et tilfeldig bilde også kaller det seg selv etter 2 sekunder
+  function changePza(img){
+    i = Math.floor(Math.random() * pictureCount) + 1;
+    str = "img/pizzas/pizza" + i + ".jpg";
+    img.src = str;
+    setTimeout(function() {
+      changePza(img);
+    }, 2000);
+  }
+
+    changePza(pzaImg);
+    changePza(pzaImg2);
+});
