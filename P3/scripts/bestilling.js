e36f4908ee347e716f27c1e65eee3eb161f9e7e1
sigrunnu <43812463+sigrunnu@users.noreply.github.com>
2018-11-09 20:07:00 +0100

    kjsdhagsdh yaay

--- P3/scripts/bestilling.js
+++ P3/scripts/bestilling.js
@@ -1,5 +1,200 @@
+const retter = [
+  {
+  pizza:'Margherita',
+  fyll:'Tomatsaus, fersk mozarella, basilikum',
+  pris: 150
+},
+{
+  pizza: 'Vesuvio',
+  fyll:'Tomatsaus, ost, skinke',
+  pris: 170
+},
+{
+  pizza: 'Parma',
+  fyll:'Parmaskinke, løk, parmesan, pesto',
+  pris: 180
+},
+{
+  pizza: 'Napoli',
+  fyll:'Pepperoni, paprika, ananas',
+  pris: 180
+},
+{
+  pizza: 'Hawaii',
+  fyll:'Skinke, ananas',
+  pris: 170
+},
+{
+  pizza: 'Singsaker',
+  fyll:'Kjøttdeig, løk, paprika, champignon',
+  pris: 190
+},
+{
+  pizza: 'Mjøndalen Spesial',
+  fyll: 'Biff, pepperoni, paprika',
+  pris: 200
+},
+{
+  pizza: 'Kebabpizza',
+  fyll:  'Biff, pepperoni, paprika',
+  pris: 190
+},
+{
+  pizza:'Vegetar',
+  fyll:'Paprika, ananas, champignon, løk, oliven',
+  pris: 160
+},
+{
+  pizza:'Tacokylling',
+  fyll: 'Krydret kylling, løk, jalapeno, nachochips ',
+  pris: 200
+}
+];
+;
+
+let meny = document.getElementById("pizza");
+let liste = document.getElementById('liste');
 
 
+for (let i = 0; i < retter.length; i++) {
+  meny.innerHTML +=
+  `<hr>
+  <h2>`+ retter[i].pizza + `</h2>
+  <p>`+ retter[i].fyll + `</p>
+  <p class='pris'>`+ (retter[i].pris+',-') + `</p>
+  <button class='velg' id='velg`+ i + `''>Velg<i class='fa fa-shopping-cart'></i></button>
+  <button class='angre' id='angre`+i+`'><i class='fa fa-close'></i> Fjern</button>`
+
+  /*document.getElementById('velg'+i).addEventListener('click', function() {
+          console.log("test")
+          let liste = document.getElementById('liste');
+          liste.innerHTML+="<li id= 'pizza"+i+"'>"+ retter[i].pizza +"</li>"
+        });
+
+I teorien børe dette funke men det gjør det ikke,
+etter å ha prøvd alt for mange forskjellige alternative løsninger,
+ble det bestemt å heller skrive ut koden under. Det samme gjelder angre funksjonen som kunne vært skrevet slik:
+
+document.getElementById("angre"+i).addEventListener("click", function() {
+            let fjern = document.getElementById("pizza"+i)
+            fjern.parentNode.removeChild(fjern);
+          })
+  */
+}
+
+document.getElementById('velg0').addEventListener('click', function() {
+        liste.innerHTML+="<li id=pizza0>"+ retter[0].pizza +' '+retter[0].pris+',-'+"</li>"
+        document.getElementById('handlekurv').style.display="inline";
+      });
+
+document.getElementById('velg1').addEventListener('click', function() {
+           liste.innerHTML+="<li id=pizza1>"+ retter[1].pizza +' '+retter[1].pris+',-'+"</li>"
+             document.getElementById('handlekurv').style.display="inline";
+
+      });
+
+document.getElementById('velg2').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza2>"+ retter[2].pizza +' '+retter[2].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+        });
+document.getElementById('velg3').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza3>"+ retter[3].pizza +' '+retter[3].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+        });
+document.getElementById('velg4').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza4>"+ retter[4].pizza +' '+retter[4].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById('velg5').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza5>"+ retter[5].pizza +' '+retter[5].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById('velg6').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza6>"+ retter[6].pizza +' '+retter[6].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById('velg7').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza7>"+ retter[7].pizza +' '+retter[7].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById('velg8').addEventListener('click', function() {
+            liste.innerHTML+="<li id=pizza8>"+ retter[8].pizza +' '+retter[8].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById('velg9').addEventListener('click', function() {
+            liste.innerHTML+="<li id='pizza9'>"+retter[9].pizza+' '+retter[9].pris+',-'+"</li>"
+              document.getElementById('handlekurv').style.display="inline";
+          });
+
+document.getElementById("angre0").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza0")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre1").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza1")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre2").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza2")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre3").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza3")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre4").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza4")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre5").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza5")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre6").addEventListener("click", function() {
+            let fjern = document.getElementById("pizza6")
+            fjern.parentNode.removeChild(fjern);
+          })
+
+document.getElementById("angre7").addEventListener("click", function() {
+            let item = document.getElementById("pizza7")
+            item.parentNode.removeChild(item);
+          })
+
+document.getElementById("angre8").addEventListener("click", function() {
+            let item = document.getElementById("pizza8")
+            item.parentNode.removeChild(item);
+          })
+
+document.getElementById("angre9").addEventListener("click", function() {
+              let item = document.getElementById("pizza9")
+              item.parentNode.removeChild(item);
+      })
+
+
+
+      /* Send bestilling */
+
+      function sendbestilling(){
+        alert('Din bestilling er sendt!')
+        window.location.reload(true);
+        document.getElementById('handlekurv').style.display="none";
+      }
+      document.getElementById('send').addEventListener('click',sendbestilling);
+
+
+
+/*
 const retter=[
   {
   pizza:'Margherita',
@@ -57,7 +252,7 @@ const retter=[
 /*lager menyen, 10 elementer */
 
 
-
+/*
 for ( var i=0; i<10; i++){
 
   var tittel=document.createElement("h2");
@@ -93,38 +288,22 @@ for ( var i=0; i<10; i++){
 
 }
 
-/* Send bestilling */
-function sendbestilling(){
-  alert('Din bestilling er sendt!')
-  window.location.reload(true);
-  document.getElementById('handlekurv').style.display="none";
-}
-document.getElementById('send').addEventListener('click',sendbestilling);
 
-liste1=[1,2,3,4]
 
 /*Bestill */
-for ( var i=0; i<10; i++){
-  document.getElementById('add'+(i+1)).addEventListener("click", lagListe);
-}
 /*
-function lagListe() {
-    document.getElementById('handlekurv').style.display="inline";
-    var li=document.createElement('li');
-    liste.appendChild(li);
-      for (var el in liste1){
-        li.innerHTML+= (liste1[el]);
-*/
-function lagListe() {
-    document.getElementById('handlekurv').style.display="inline";
-    var li=document.createElement('li');
-    liste.appendChild(li);
-
-        li.innerHTML+=(retter[i].pizza);
-
+for ( var i=0; i<10; i++){
+  document.getElementById('add'+(i+1)).addEventListener("click", function(){lagListe(i)});
 }
 
 
+function lagListe(i) {
+    document.getElementById('handlekurv').style.display="inline";
+    var li=document.createElement('li');
+    liste.appendChild(li);
+    li.innerHTML+=(retter[i].pizza);
+
+}
   /*  createShoppingElement(i)
   }
 
--- P3/scripts/script.js
+++ P3/scripts/script.js
@@ -1,4 +1,5 @@
 
+//Skjema til kontakt oss//
 
 function sendMail(){
   let name = document.getElementById("navn");
@@ -6,18 +7,103 @@ function sendMail(){
   let comment = document.getElementById("kommentar");
 
   if (name.value == ""){
-    alert("Du må skrive inn navn for å sende mailen");
+    alert("Du må skrive inn navn for å sende E-posten");
   }
 
   else if(mail.value == ""){
-    alert("Du må skrive inn enn email");
+    alert("Du må skrive inn en E-postadresse");
   }
 
   else if (comment.value == ""){
-    alert("Mailen din må inneholde en komentar");
+    alert("Mailen din må inneholde en kommentar");
   }
 
   else{
-    alert("Emailen er sendt!");
+    alert("E-posten er sendt!");
+      window.location.reload(true);
   }
 }
+//Skjema til søkjobb//
+
+
+function sendSøknad(){
+  let navn1 = document.getElementById("navn1");
+  let epost = document.getElementById("E-post");
+  let kommentar = document.getElementById("søknadsTekst");
+  let føds=document.getElementById('Fødselsdato')
+  let fil=document.getElementById('fileupload')
+
+  if (navn1.value == ""){
+    alert("Du må skrive inn navn for å sende inn søknad");
+  }
+
+  else if(epost.value == ""){
+    alert("Du må skrive inn en E-postadresse");
+  }
+  else if (føds.value == ""){
+    alert("Du må oppgi fødselsdatoen din");
+  }
+
+  else if (kommentar.value == ""){
+    alert("Søknaden din må inneholde en kommentar");
+  }
+
+  else if (fil.value == ""){
+    alert("Du må laste opp en CV");
+  }
+  else{
+    alert("Søknaden er sendt!");
+      window.location.reload(true);
+  }
+
+}
+
+document.getElementById("navigasjonsmeny").innerHTML =(`<div class="meny">
+
+<div>
+
+  <a href="hjem.html"><input type="image" id="logo2" alt="Login" src="img/logopizza.png"></input></a>
+
+                  <ul id="headerul">
+                      <a href="hjem.html"><input type="image" id="logo" alt="Login" src="img/logopizza.png"></input></a>
+                      <li class="headerli"><a href="Meny.html">Meny/bestill</a></li>
+                      <li class="headerli"><a href="omoss.html">Om restauranten</a></li>
+                      <li class="headerli"><a href="kontakt.html">Ta kontakt</a></li>
+                      <li class="headerli"><a href="sok_jobb.html">Søk jobb</a></li>
+                  </ul>
+
+                  <div class="dropdown">
+                    <button class="dropbtn"><i class="fa fa-bars"></i></button>
+                        <div class="dropdown-content">
+                  <a href="Meny.html">Meny/bestill</a>
+                    <a href="omoss.html">Om resturanten</a>
+                    <a href="kontakt.html">Ta kontakt</a>
+                      <a href="sok_jobb.html">Søk jobb</a>
+                  </div>
+                </div>
+                </div>
+                  <hr id="menylinje">
+                        </div>
+                  </div>`)
+
+document.getElementById("footer").innerHTML = (`<footer>
+    <hr id="footerlinje">
+  <ul id="footerul">
+    <li class="footerli">
+      Kontakt oss:
+    </li>
+    <br />
+    <li class="footerli">
+      Epostadresse: pizza@martin.lange.no
+    </li>
+    <li class="footerli">
+      tlf: +47 959 40 838
+    </li>
+    <li class="footerli">
+      Facebook: @Pizzastekeren
+    </li>
+    <li class="footerli">
+
+    </li>
+  </ul>
+</footer>`);
